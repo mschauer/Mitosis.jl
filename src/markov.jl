d8f295b0879124ffe280010345ba5d7dfb15079b
mschauer <moritzschauer@web.de>
2020-10-09 19:19:41 +0200

    Moved to Measuretheory.jl

--- src/markov.jl
+++ src/markov.jl
@@ -1,8 +1,2 @@
-struct MarkovKernel{T,S}
-    ops::S
-    m::T
-end
-(k::MarkovKernel)(x) = k.m((op(x) for op in k.ops)...)
-
-const GaussKernel = MarkovKernel{<:Gaussian}
-const Copy = MarkovKernel{<:Dirac}
+const GaussKernel = MeasureTheory.Kernel{<:Gaussian}
+const Copy = MeasureTheory.Kernel{<:Dirac}
