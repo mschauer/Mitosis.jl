5c188a4973e8a5fd9441f74dabbcf2ae68a05828
mschauer <moritzschauer@web.de>
2020-11-16 12:48:41 +0100

    Updates

--- example/bm.jl
+++ example/bm.jl
@@ -1,6 +1,45 @@
 # Fit a Brownian motion with drift to and scale to
 # Georgia election data:
 # fraction of total count of votes vs difference in votes
+using DelimitedFiles
+datafile = download("https://alex.github.io/nyt-2020-election-scraper/battleground-state-changes.csv")
+datatable, header = readdlm(datafile, ',', header=true)
+
+ii = datatable[:,1] .== "Pennsylvania (EV: 20)"
+
+ss = 2(datatable[ii,3] .== "Trump") .- 1
+c = reverse(datatable[ii,5] + datatable[ii,6])
+x = reverse(datatable[ii,7].*ss)
+
+N = c[end]
+# N = C + J
+R = datatable[ii, 8][1]
+T = (N - R)/N # remaining percentage to count
+X = x[end] #vote difference
+
+t = c ./ (R + N)
+
+#=
+(1, "state")
+(2, "timestamp")
+(3, "leading_candidate_name")
+(4, "trailing_candidate_name")
+(5, "leading_candidate_votes")
+(6, "trailing_candidate_votes")
+(7, "vote_differential")
+(8, "votes_remaining")
+(9, "new_votes")
+(10, "new_votes_relevant")
+(11, "new_votes_formatted")
+(12, "leading_candidate_partition")
+(13, "trailing_candidate_partition")
+(14, "precincts_reporting")
+(15, "precincts_total")
+(16, "hurdle")
+(17, "hurdle_change")
+(18, "hurdle_mov_avg")
+(19, "counties_partition")
+(20, "total_votes_count")
 
 data = [0.9222672139181572 103896.10389610389
     0.9267802009311443 87662.33766233765
@@ -21,8 +60,11 @@ data = [0.9222672139181572 103896.10389610389
     0.9526721391815731 33116.88311688311
     0.9532369517275178 31818.18181818181
     0.9578473413379074 18831.16883116882]
-t = data[:, 1]
-x = data[:, 2]
+    t = data[:, 1]
+    x = data[:, 2]
+
+=#
+
 
 using Mitosis
 using Random, Test, LinearAlgebra, Statistics
@@ -52,8 +94,10 @@ function gibbs(t, x, iterations)
         end
 
         # conjugate σ2
-        α = α0
-        β = β0
+        #α = α0
+        #β = β0
+        α = 2n
+        β = var(Binomial(N, (1-μ/N)/2))*α
         μ = rand(convert(Gaussian{(:μ,:Σ)}, p))[]
         for i in n-1:-1:1
             Δ = t[i+1] - t[i]
@@ -63,7 +107,8 @@ function gibbs(t, x, iterations)
             α = α + 1/2
             β = β + (x[i+1] - mean(k(x[i])))^2/(2Δ)
         end
-        σ = sqrt(rand(InverseGamma(α, β)))
+        #σ = sqrt(rand(InverseGamma(α, β)))
+        σ = std(Binomial(N, (1-μ/N)/2))
         push!(out, (μ, σ))
     end
     out
@@ -83,28 +128,51 @@ using Makie
 
 pl1 = hbox(lines(getindex.(chain, 1)),
    lines(getindex.(chain, 2)))
-# Make a small prediction
-
-J = 2439809 # incumbent
-C = 2430275 # challenger
-N = C + J
-T = (N - 36000)/N # remaining percentage to count
-X = J - C # vote difference
 
 s = range(T, 1.00-eps(), length=100)
-μu = quantile(μs, 0.90)
-μl = quantile(μs, 0.10)
+μu = quantile(μs, 0.80)
+μl = quantile(μs, 0.20)
 μ = median(μs)
-σu = quantile(σs, 0.9)
+σ = median(σs)
+
+σu = quantile(σs, 0.8)
 m = X .+ (s .- T)*μ
 u = X .+ (s .- T)*μu .+ 2σu*sqrt.(s .- T)
 l = X .+ (s .- T)*μl .- 2σu*sqrt.(s .- T)
 Scene(resolution = (600, 600), textsize=20)
 scatter!([T], [X])
-lines!(s, u)
+#lines!(s, u)
 lines!(s, m)
-lines!(s, l)
+#lines!(s, l)
 pl2 = band!(s, l, u)
 
 
+v = [rand(kernel(Gaussian; μ=x->x+rand(μs)*(1-T), Σ=_->rand(σs)^2*(1-T))(X))
+   for k in 1:1000]
+mean(v .> 0)
+mean(u[end] .> v .> l[end])
+#scatter!(one.(v)+0.00001*randn(length(v)), v, markersize=0.1)
 pl2
+
+
+function bm(x, s, μ, σ)
+    y = float(x)
+    ys = [y]
+    for i in 1:length(s)-1
+        Δ = s[i+1] - s[i]
+        r = Mitosis.Regression([μ], (ConstantMap(Δ),), identity)
+        k = kernel(Gaussian; μ=r, Σ=ConstantMap(σ^2*Δ))
+        y = rand(k(y))
+        push!(ys, y)
+    end
+    ys
+end
+
+for i in 1:100
+    lines!(s, bm(X, s, rand(μs), rand(σs)))
+end
+pl2
+
+
+#
+σ = std(Binomial(N, (1-μ/N)/2))
